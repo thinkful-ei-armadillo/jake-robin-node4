a1b25cbcbf4a8e48d5eca6fbcf991c3051458b46
Juicebox Jones <jderhalli@gmail.com>
2019-02-22 09:19:43 -0800

    initial commit

--- src/app.js
+++ src/app.js
@@ -6,15 +6,120 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const app = express();
+const UUID = require('uuid/v4');
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+
+app.use((req,res,next) => {
+  const authToken = req.get('Authorization');
+
+  if(!authToken || authToken.split(' ')[1] !== process.env.API_TOKEN){
+    return res.status(401).json({ error: 'Unauthorized'});
+  }
+  next();
+});
+
+const users = [
+  {
+    'id': '3c8da4d5-1597-46e7-baa1-e402aed70d80',
+    'username': 'sallyStudent',
+    'password': 'c00d1ng1sc00l',
+    'favoriteClub': 'Cache Valley Stone Society',
+    'newsLetter': 'true'
+  },
+  {
+    'id': 'ce20079c-2326-4f17-8ac4-f617bfd28b7f',
+    'username': 'johnBlocton',
+    'password': 'veryg00dpassw0rd',
+    'favoriteClub': 'Salt City Curling Club',
+    'newsLetter': 'false'
+  }
+];
+
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('POST request recieved.');
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('A GET Request');
+});
+
+app.get('/user/:userId', (req, res) => {
+  const { userId } = req.params;
+  console.log(userId);
+  res.send('check the console');
+});
+
+app.get('/user', (req,res) => {
+  res.json(users);
+});
+
+app.delete('/user/:userId', (req, res) => {
+  const { userId } = req.params;
+  const index = users.findIndex(u => u.id === userId);
+  if(index === -1) {
+    return res.status(404).send('User not found');
+  }
+  users.splice(index, 1);
+  res.status(204).end();
+});
+
+app.post('/register', (req, res) => {
+  const { username, password, favoriteClub, newsLetter=false } = req.body;
+
+  if(!username) {
+    return res.status(400).send('Username Required');
+  }  
+  if(!password){
+    return res.status(400).send('Password Required');
+  }
+  if(!favoriteClub){
+    return res.status(400).send('Favorite Club Required');
+  }
+  if (username.length < 6 || username.length > 20 ) {
+    return res.status(400).send('username must be between 6 and 20 characters');
+  }
+  if (password.length < 8 || password.length > 36 ) {
+    return res.status(400).send('password must be between 8 and 36 characters');
+  }  
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res
+      .status(400)
+      .send('Password must be contain at least one digit');
+  }
+
+  const clubs = [
+    'Cache Valley Stone Society',
+    'Ogden Curling Club',
+    'Park City Curling Club',
+    'Salt City Curling Club',
+    'Utah Olympic Oval Curling Club'
+  ];
+
+  if(!clubs.includes(favoriteClub)){
+    return res.status(400).send('Not a valid club');
+  }
+
+  const id = UUID();
+  const newUser = {
+    id,
+    username,
+    password,
+    favoriteClub,
+    newsLetter
+  };
+
+  users.push(newUser);
+
+  res.status(201).location(`http://localhost:8000/user/${id}`).json({id});
+
 });
 
 
